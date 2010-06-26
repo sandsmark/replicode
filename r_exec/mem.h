9d2d4793d90291316ededf862daf2c3f2f073c86
eric@ru.is <eric@ru.is@36b273a1-5dce-4f9d-8980-b90fca7d7570>
2010-06-26 11:33:37 +0000

    Added file headers. Delegate cores are now properly deallocated.
    git-svn-id: http://cadia.ru.is/svn/repos/replicode@166 36b273a1-5dce-4f9d-8980-b90fca7d7570

--- r_exec/mem.h
+++ r_exec/mem.h
@@ -1,3 +1,33 @@
+//	mem.h
+//
+//	Author: Eric Nivel
+//
+//	BSD license:
+//	Copyright (c) 2008, Eric Nivel
+//	All rights reserved.
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are met:
+//
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   - Neither the name of Eric Nivel nor the
+//     names of their contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//	DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef	mem_h
 #define	mem_h
 
@@ -33,7 +63,7 @@ namespace	r_exec{
 		uint32			time_core_count;
 		TimeCore		**time_cores;
 
-		std::vector<DelegatedCore	*>	delegates;
+		std::list<DelegatedCore	*>	delegates;
 
 		State			state;
 		FastSemaphore	*state_sem;
@@ -65,6 +95,7 @@ namespace	r_exec{
 		uint64	get_base_period()	const{	return	base_period;	}
 
 		void	add_delegate(uint64	dealine,_TimeJob	*j);
+		void	remove_delegate(DelegatedCore	*core);
 		void	stop();	//	after stop() the content is cleared and one has to call load() and start() again.
 
 		State	get_state();
