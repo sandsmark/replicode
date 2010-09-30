c7e2b53dbd2793c84a581e921ec67c2db3efca9c
eric@ru.is <eric@ru.is@36b273a1-5dce-4f9d-8980-b90fca7d7570>
2010-09-30 10:44:13 +0000

    git-svn-id: http://cadia.ru.is/svn/repos/replicode@225 36b273a1-5dce-4f9d-8980-b90fca7d7570

--- r_exec/time_job.cpp
+++ r_exec/time_job.cpp
@@ -1,44 +1,63 @@
+//	time_job.cpp
+//
+//	Author: Eric Nivel
+//
+//	BSD license:
+//	Copyright (c) 2010, Eric Nivel
+//	All rights reserved.
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are met:
+//
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   - Neither the name of Eric Nivel nor the
+//     names of their contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//	DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #include	"time_job.h"
 #include	"mem.h"
 
 
 namespace	r_exec{
 
-	bool	_TimeJob::is_alive()	const{
+	TimeJob::TimeJob(uint64	ijt):_Object(),target_time(ijt){
+	}
+
+	bool	TimeJob::is_alive()	const{
 
 		return	true;
 	}
 
 	////////////////////////////////////////////////////////////
 
-	TimeJob::TimeJob(){
-	}
-
-	TimeJob::TimeJob(_TimeJob	*j,uint64	ijt):target_time(ijt){
-
-		job=j;
-	}
-
-	bool	TimeJob::is_alive()	const{
-
-		return	job->is_alive();
-	}
-
-	////////////////////////////////////////////////////////////
-
-	UpdateJob::UpdateJob(Group	*g){
+	UpdateJob::UpdateJob(Group	*g,uint64	ijt):TimeJob(ijt){
 
 		group=g;
 	}
 
-	void	UpdateJob::update(Mem	*m){
+	bool	UpdateJob::update(_Mem	*m){
 
-		m->update(this);
+		m->update(group);
+		return	true;
 	}
 
 	////////////////////////////////////////////////////////////
 
-	SignalingJob::SignalingJob(IPGMController	*o){
+	SignalingJob::SignalingJob(Controller	*o,uint64	ijt):TimeJob(ijt){
 
 		controller=o;
 	}
@@ -50,45 +69,101 @@ namespace	r_exec{
 
 	////////////////////////////////////////////////////////////
 
-	AntiPGMSignalingJob::AntiPGMSignalingJob(IPGMController	*o):SignalingJob(o){
+	AntiPGMSignalingJob::AntiPGMSignalingJob(AntiPGMController	*o,uint64	ijt):SignalingJob(o,ijt){
 	}
 
-	void	AntiPGMSignalingJob::update(Mem	*m){
+	bool	AntiPGMSignalingJob::update(_Mem	*m){
 
-		m->update(this);
-	}
-
-	////////////////////////////////////////////////////////////
-
-	InputLessPGMSignalingJob::InputLessPGMSignalingJob(IPGMController	*o):SignalingJob(o){
-	}
-
-	void	InputLessPGMSignalingJob::update(Mem	*m){
-
-		m->update(this);
+		if(controller->is_alive())
+			((AntiPGMController	*)controller)->signal_anti_pgm();
+		return	true;
 	}
 
 	////////////////////////////////////////////////////////////
 
-	InjectionJob::InjectionJob(View	*v){
+	InputLessPGMSignalingJob::InputLessPGMSignalingJob(InputLessPGMController	*o,uint64	ijt):SignalingJob(o,ijt){
+	}
+
+	bool	InputLessPGMSignalingJob::update(_Mem	*m){
+
+		if(controller->is_alive())
+			((InputLessPGMController	*)controller)->signal_input_less_pgm();
+		return	true;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	InjectionJob::InjectionJob(View	*v,uint64	ijt):TimeJob(ijt){
 
 		view=v;
 	}
 
-	void	InjectionJob::update(Mem	*m){
+	bool	InjectionJob::update(_Mem	*m){
 
-		m->update(this);
+		m->injectNow(view);
+		return	true;
 	}
 
 	////////////////////////////////////////////////////////////
 
-	SaliencyPropagationJob::SaliencyPropagationJob(Object	*o,float32	sln_change,float32	source_sln_thr):sln_change(sln_change),source_sln_thr(source_sln_thr){
+	EInjectionJob::EInjectionJob(View	*v,uint64	ijt):TimeJob(ijt){
+
+		view=v;
+	}
+
+	bool	EInjectionJob::update(_Mem	*m){
+
+		m->injectExistingObjectNow(view,view->object,view->get_host(),true);
+		return	true;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	GInjectionJob::GInjectionJob(View	*v,Group	*g,Group	*h,uint64	ijt):TimeJob(ijt){
+
+		view=v;
+		group=g;
+		host=h;
+	}
+
+	bool	GInjectionJob::update(_Mem	*m){
+
+		m->injectGroupNow(view,group,host);
+		return	true;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	SaliencyPropagationJob::SaliencyPropagationJob(Code	*o,float32	sln_change,float32	source_sln_thr,uint64	ijt):TimeJob(ijt),sln_change(sln_change),source_sln_thr(source_sln_thr){
 
 		object=o;
 	}
 	
-	void	SaliencyPropagationJob::update(Mem	*m){
+	bool	SaliencyPropagationJob::update(_Mem	*m){
 
-		m->update(this);
+		if(!object->is_invalidated())
+			m->propagate_sln(object,sln_change,source_sln_thr);
+		return	true;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	ShutdownTimeCore::ShutdownTimeCore():TimeJob(0){
+	}
+
+	bool	ShutdownTimeCore::update(_Mem	*m){
+
+		m->shutdown_core();
+		return	false;
+	}
+
+	////////////////////////////////////////////////////////////
+
+	SuspendTimeCore::SuspendTimeCore():TimeJob(0){
+	}
+
+	bool	SuspendTimeCore::update(_Mem	*m){
+
+		return	m->suspend_core();
 	}
 }
--- r_exec/time_job.h
+++ r_exec/time_job.h
@@ -1,77 +1,131 @@
+//	time_job.h
+//
+//	Author: Eric Nivel
+//
+//	BSD license:
+//	Copyright (c) 2010, Eric Nivel
+//	All rights reserved.
+//	Redistribution and use in source and binary forms, with or without
+//	modification, are permitted provided that the following conditions are met:
+//
+//   - Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//   - Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   - Neither the name of Eric Nivel nor the
+//     names of their contributors may be used to endorse or promote products
+//     derived from this software without specific prior written permission.
+//
+//	THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND ANY
+//	EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//	WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//	DISCLAIMED. IN NO EVENT SHALL THE REGENTS AND CONTRIBUTORS BE LIABLE FOR ANY
+//	DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//	(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//	LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+//	ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//	(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+//	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #ifndef	time_job_h
 #define	time_job_h
 
-#include	"object.h"
+#include	"group.h"
 #include	"pgm_overlay.h"
 
 
 namespace	r_exec{
-
-	class	Mem;
-
-	class	_TimeJob:
-	public	r_code::_Object{
+	
+	class	r_exec_dll	TimeJob:
+	public	_Object{
+	protected:
+		TimeJob(uint64	ijt);
 	public:
-		virtual	void	update(Mem	*m)=0;
+		int64			target_time;		//	0 means ASAP.
+		virtual	bool	update(_Mem	*m)=0;	//	return false to shutdown the time core.
 		virtual	bool	is_alive()	const;
 	};
-	
-	class	TimeJob{
+
+	class	r_exec_dll	UpdateJob:
+	public	TimeJob{
 	public:
-		int64				target_time;	//	0 means ASAP.
-		r_code::P<_TimeJob>	job;
-		TimeJob();
-		TimeJob(_TimeJob	*j,uint64	ijt);
-		bool	is_alive()	const;
+		P<Group>	group;
+		UpdateJob(Group	*g,uint64	ijt);
+		bool	update(_Mem	*m);
 	};
 
-	class	UpdateJob:
-	public	_TimeJob{
-	public:
-		r_code::P<Group>	group;
-		UpdateJob(Group	*g);
-		void	update(Mem	*m);
-	};
-
-	class	SignalingJob:
-	public	_TimeJob{
+	class	r_exec_dll	SignalingJob:
+	public	TimeJob{
 	protected:
-		SignalingJob(IPGMController	*o);
+		SignalingJob(Controller	*o,uint64	ijt);
 	public:
-		r_code::P<IPGMController>	controller;
+		P<Controller>	controller;
 		bool	is_alive()	const;
 	};
 
-	class	AntiPGMSignalingJob:
+	class	r_exec_dll	AntiPGMSignalingJob:
 	public	SignalingJob{
 	public:
-		AntiPGMSignalingJob(IPGMController	*o);
-		void	update(Mem	*m);
+		AntiPGMSignalingJob(AntiPGMController	*o,uint64	ijt);
+		bool	update(_Mem	*m);
 	};
 
-	class	InputLessPGMSignalingJob:
+	class	r_exec_dll	InputLessPGMSignalingJob:
 	public	SignalingJob{
 	public:
-		InputLessPGMSignalingJob(IPGMController	*o);
-		void	update(Mem	*m);
+		InputLessPGMSignalingJob(InputLessPGMController	*o,uint64	ijt);
+		bool	update(_Mem	*m);
 	};
 
-	class	InjectionJob:
-	public	_TimeJob{
+	class	r_exec_dll	InjectionJob:
+	public	TimeJob{
 	public:
-		r_code::P<View>	view;
-		InjectionJob(View	*v);
-		void	update(Mem	*m);
+		P<View>	view;
+		InjectionJob(View	*v,uint64	ijt);
+		bool	update(_Mem	*m);
 	};
 
-	class	SaliencyPropagationJob:
-	public	_TimeJob{
+	class	r_exec_dll	EInjectionJob:
+	public	TimeJob{
 	public:
-		r_code::P<Object>		object;
-		float32					sln_change;
-		float32					source_sln_thr;
-		SaliencyPropagationJob(Object	*o,float32	sln_change,float32	source_sln_thr);
-		void	update(Mem	*m);
+		P<View>	view;
+		EInjectionJob(View	*v,uint64	ijt);
+		bool	update(_Mem	*m);
+	};
+
+	class	r_exec_dll	GInjectionJob:
+	public	TimeJob{
+	public:
+		P<View>		view;
+		P<Group>	group;
+		P<Group>	host;
+		GInjectionJob(View	*v,Group	*g,Group	*h,uint64	ijt);
+		bool	update(_Mem	*m);
+	};
+
+	class	r_exec_dll	SaliencyPropagationJob:
+	public	TimeJob{
+	public:
+		P<Code>	object;
+		float32		sln_change;
+		float32		source_sln_thr;
+		SaliencyPropagationJob(Code	*o,float32	sln_change,float32	source_sln_thr,uint64	ijt);
+		bool	update(_Mem	*m);
+	};
+
+	class	r_exec_dll	ShutdownTimeCore:
+	public	TimeJob{
+	public:
+		ShutdownTimeCore();
+		bool	update(_Mem	*m);
+	};
+
+	class	r_exec_dll	SuspendTimeCore:
+	public	TimeJob{
+	public:
+		SuspendTimeCore();
+		bool	update(_Mem	*m);
 	};
 }
 
